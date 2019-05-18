63decf38e33e17746233caf145b0f3228b48c46e
lesslate <39299919+lesslate@users.noreply.github.com>
2019-05-19 00:20:56 +0900

    Assert

--- Source/Raid/Raid.cpp
+++ Source/Raid/Raid.cpp
@@ -3,5 +3,5 @@
 #include "Raid.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(Raid);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Raid, "Raid" );
- 
--- Source/Raid/Raid.h
+++ Source/Raid/Raid.h
@@ -3,3 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(Raid, Log, All);
+
+#define LOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define LOG_S(Verbosity) UE_LOG(Raid, Verbosity, TEXT("%s"), *LOG_CALLINFO)
+#define LOG(Verbosity, Format, ...) UE_LOG(Raid, Verbosity, TEXT("%s %s"), *LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+#define CHECK(Expr, ...) {if(!(Expr)) {LOG(Error, TEXT("ASSERTION : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__;}}
+
